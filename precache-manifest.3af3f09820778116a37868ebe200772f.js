8dcc7193bfbf2aeb92df81cbca06360803cf97e3
Shahul Hameed <beingshahul@gmail.com>
2019-10-25 15:43:16 +0530

    Updates

---
+++ precache-manifest.3af3f09820778116a37868ebe200772f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b740e8d161a8c6f30066e8f6e0a8ce00",
+    "url": "/test-react-deploy/index.html"
+  },
+  {
+    "revision": "51f3d68421fdb07d8144",
+    "url": "/test-react-deploy/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/test-react-deploy/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "51f3d68421fdb07d8144",
+    "url": "/test-react-deploy/static/js/main.53aeaa10.chunk.js"
+  },
+  {
+    "revision": "fe204b40689f367a3f19",
+    "url": "/test-react-deploy/static/js/runtime~main.a2707ced.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
